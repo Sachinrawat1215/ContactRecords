14038ba4153c656e34860167a8e57f028d7ae5af
Sachinrawat1215 <sachinrawat0853@gmail.com>
2021-09-12 20:29:23 +0530

    sorting work completed and search functionality remaining...

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let btn = document.getElementById('add-btn');
 let addCrd = document.getElementById('AddCard');
 let info = document.getElementById("info-line");
+let name = document.getElementById('name');
 let ary = [];
 let obj = {
     name: "",
     age: "",
     gender: ""
 };
-let name = document.getElementById("name");
-let age = document.getElementById("age");
-let short = document.getElementById("short");
 
 show();
+
 // Add card show 
 btn.addEventListener("click", () => {
     addCrd.style.visibility = "visible";
@@ -21,6 +20,7 @@ btn.addEventListener("click", () => {
 let hideBtn = document.getElementById('HideAdd');
 hideBtn.addEventListener("click", () => {
     addCrd.style.visibility = "hidden";
+    info.innerText = '';
 })
 
 // Adding card information 
@@ -30,40 +30,57 @@ addBtn.addEventListener('click', () => {
     let ageVal = age.value;
     let genVal = gender.value;
 
-    obj.name = nameVal;
-    obj.age = ageVal;
-    obj.gender = genVal;
+    if (nameVal === "") {
+        info.innerText = "Enter name...";
+    } else if (ageVal === "") {
+        info.innerText = "Enter age..."
+    } else {
+        obj.name = nameVal;
+        obj.age = ageVal;
+        obj.gender = genVal;
 
-    ary.push(obj);
-    localStorage.setItem("information", JSON.stringify(ary));
-    info.innerText = "Contact added sucessfully...";
-
+        ary.push(obj);
+        localStorage.setItem("information", JSON.stringify(ary));
+        info.innerText = "Contact added sucessfully...";
+    }
     show();
-
     name.value = "";
     age.value = "";
 });
 
 function show() {
-
     let detail = localStorage.getItem("information");
-    // let main = obj[index].name; 
-
     if (detail == null) {
         ary = [];
     } else {
         ary = JSON.parse(detail);
     };
+    
     let html = "";
-    ary.forEach((element, index) => {
-        html += `
-        <div class="user-detail" >
-        <div class="serial-number">${index + 1}</div> 
-        <div class="user-name">${element.name}</div> 
-        <div class="user-age">${element.age}</div> 
-        <div class="user-gender">${element.gender}</div>
-        </div>`
-    });
+    function common(){
+        ary.forEach((element, index) => {
+            html += `
+            <div class="user-detail" >
+            <div class="serial-number">${index + 1}</div> 
+            <div class="user-name">${element.name}</div> 
+            <div class="user-age">${element.age}</div> 
+            <div class="user-gender">${element.gender}</div>
+            </div>`
+        })
+    };
+
+    if (short.value == "A - Z") {
+        ary.sort((firstElement, secondElement) => (firstElement.name > secondElement.name ? 1 : -1));
+        common();
+    } else if (short.value == "Z - A") {
+        ary.sort((firstElement, secondElement) => (firstElement.name > secondElement.name ? -1 : 1));
+        common();
+    } else if (short.value == "Age") {
+        ary.sort((firstElement, secondElement) => (firstElement.age > secondElement.age ? 1 : -1));
+        common();
+    } else {
+        common();
+    }
 
     let detailElm = document.getElementById("outer");
     if (ary.lenght != 0) {
@@ -71,32 +88,6 @@ function show() {
     } else {
         detailElm.innerHTML = "Nothing to show add contact first";
     };
-
 };
 
-// Short function Coding
-
-function shorting() {
-
-    // // console.log(ary.obj.name);
-    // ary.forEach((element,index) => {
-    //     let name = element.name;
-    //     let nameAry = [];
-    //     nameAry.push(name);
-    //     console.log(nameAry);
-    // })
-
-    // if (short.value = "A - Z") {
-        let detail = localStorage.getItem("information");
-        ary = JSON.parse(detail);
-        let sortingarray;
-
-        ary.forEach((element) => {
-            sortingarray += element.name
-        });
-        sortingarray.sort();
-    // }
-
-};
-
-shorting();
+short.addEventListener('change', show);
